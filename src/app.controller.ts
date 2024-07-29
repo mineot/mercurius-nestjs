dc981b65b051d21b211d8e595328c81a20215832
mineot <mineot@gmail.com>
2024-07-29 00:36:06 -0300

    alpha/prisma - create schema, first table and module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prisma: PrismaService,
+  ) {}
 
   @Get()
   getHello(): string {
+    this.prisma.user.findFirst().then((user: any) => {
+      console.log(user);
+    });
     return this.appService.getHello();
   }
 }
