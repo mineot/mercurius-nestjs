f4dc1e815fa444b45b55846f2da1009eb2284f96
mineot <mineot@gmail.com>
2024-07-29 01:15:42 -0300

    alpha/prisma - rename module prisma to shared

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [SharedModule],
   controllers: [AppController],
   providers: [AppService],
 })
