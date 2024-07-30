f069b175c69c0855d7074181fe40e41628094ca7
mineot <mineot@gmail.com>
2024-07-29 21:33:59 -0300

    alpha/template - create template modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { AdminModule } from './admin/admin.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { PublicModule } from './public/public.module';
 import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [SharedModule],
+  imports: [SharedModule, AdminModule, AuthModule, PublicModule],
   controllers: [AppController],
   providers: [AppService],
 })
