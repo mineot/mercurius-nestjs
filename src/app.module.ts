59f1ee38c4bd71ac56a2905374e6455f110b366f
mineot <mineot@gmail.com>
2024-07-31 21:50:09 -0300

    alpha/template - alias and configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AdminModule } from './admin/admin.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
-import { PublicModule } from './public/public.module';
-import { SharedModule } from './shared/shared.module';
+import { AppController } from '@src/app.controller';
+import { AppService } from '@src/app.service';
+
+import { AdminModule } from '@admin/admin.module';
+import { AuthModule } from '@auth/auth.module';
+import { PublicModule } from '@public/public.module';
+import { SharedModule } from '@shared/shared.module';
 
 @Module({
   imports: [SharedModule, AdminModule, AuthModule, PublicModule],
