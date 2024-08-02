98c092f452dab13b9099449797533cee0c2f163b
mineot <mineot@gmail.com>
2024-08-02 19:37:13 -0300

    alpha/profile - some tests

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,61 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Language } from '@prisma/client';
 
 describe('AppController', () => {
   let appController: AppController;
+  let appService: AppService;
 
   beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
+    const module: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
       providers: [AppService],
     }).compile();
 
-    appController = app.get<AppController>(AppController);
+    appController = module.get<AppController>(AppController);
+    appService = module.get<AppService>(AppService);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+  it('should return a profile when language is provided', async () => {
+    const mockLanguage: Language = {
+      id: 'ABC123',
+      lang: 'en',
+      country: 'US',
+      name: 'English',
+    };
+
+    jest.spyOn(appService, 'index').mockResolvedValue({
+      id: 'DEF123',
+      langId: 'ABC123',
+      name: 'John Doe',
+      job_title: 'Software Engineer',
+      summary: 'Lorem ipsum...',
+      biography: 'Software engineer',
+      photo_sm: 'https://example.com/photo_sm.jpg',
+      photo_lg: 'https://example.com/photo_lg.jpg',
+      activity: 'Your activity goes here',
     });
+
+    const result = await appController.index(mockLanguage);
+
+    expect(result).toEqual({
+      id: 1,
+      name: 'John Doe',
+      bio: 'Software engineer',
+    });
+    expect(appService.index).toHaveBeenCalledWith(mockLanguage);
+  });
+
+  it('should throw an error when language is not provided', async () => {
+    const mockLanguage: Language = undefined;
+    jest
+      .spyOn(appService, 'index')
+      .mockRejectedValue(new Error('Language is required'));
+
+    await expect(appController.index(mockLanguage)).rejects.toThrow(
+      'Language is required',
+    );
+    expect(appService.index).toHaveBeenCalledWith(mockLanguage);
   });
 });
